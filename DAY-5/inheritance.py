20a8ca32581aaffec3c63497d981690fd6619a8f
Ansh Jaiswal <145288803+anshjaiswal11@users.noreply.github.com>
2026-01-28 14:16:27 +0530

    newfiles

--- DAY-5/inheritance.py
+++ DAY-5/inheritance.py
@@ -6,6 +6,8 @@ class car:
     def display_info(self):
         print("car is used for driving")
 
+
+
 class audi(car):
     def __init__(self, windows, doors, enginetype, horsepower):
         super().__init__(windows, doors, enginetype)
@@ -13,10 +15,24 @@ class audi(car):
     def display_info(self):
         print("audi is a luxury car brand")
 
+class trunk(audi):
+    def __init__(self, windows, doors, enginetype, horsepower, capacity):
+        super().__init__(windows, doors, enginetype, horsepower)
+        self.capacity = capacity
+    def display_info(self):
+        print("trunk is used to carry goods")
+
+my_trunk = trunk(2, 2, "V8", 500, "2000kg")
+my_trunk.display_info()
+
 my_car = audi(4, 4, "V6", 300)
 my_car.display_info()
-            
+
 print(my_car.horsepower)
+
+
+
+
 
 
 
@@ -34,9 +50,13 @@ class car:
 
     @classmethod
     def update_base_price(cls, new_price):
-        cls.base_price = new_price
+        cls.base_price = new_price * cls.base_price
         print(f"Base price updated to {cls.base_price}")
 my_car = car(4, 4, "150hp")
 my_car.what_base_price()
-car.update_base_price(1500000)
+car.update_base_price(1.2)
 my_car.what_base_price()
+
+
+
+
